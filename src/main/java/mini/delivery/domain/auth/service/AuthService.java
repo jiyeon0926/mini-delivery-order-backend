c8c10ad1ef09ddb14eda2364701aefd6c6916c17
jiyeon0926 <yeoni9094@naver.com>
2026-04-09 17:16:03 +0900

    refactor: 회원가입 예외 처리
    - 이미 사용 중인 이메일로 회원가입 시 409 상태코드 반환
    
    #1, #2, #3, #7

--- src/main/java/mini/delivery/domain/auth/service/AuthService.java
+++ src/main/java/mini/delivery/domain/auth/service/AuthService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import mini.delivery.domain.auth.dto.SignupResponseDto;
 import mini.delivery.domain.user.entity.User;
 import mini.delivery.domain.user.repository.UserRepository;
+import mini.delivery.global.error.CustomException;
+import mini.delivery.global.error.ErrorCode;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +19,8 @@ public class AuthService {
 
     @Transactional
     public SignupResponseDto signupCustomer(String email, String password, String nickname) {
+        validateDuplicateEmail(email);
+
         String encodedPassword = passwordEncoder.encode(password);
         User user = User.ofCustomer(email, encodedPassword, nickname);
         User savedUser = userRepository.save(user);
@@ -26,10 +30,19 @@ public class AuthService {
 
     @Transactional
     public SignupResponseDto signupOwner(String email, String password, String nickname) {
+        validateDuplicateEmail(email);
+
         String encodedPassword = passwordEncoder.encode(password);
         User user = User.ofOwner(email, encodedPassword, nickname);
         User savedUser = userRepository.save(user);
 
         return SignupResponseDto.from(savedUser);
+    }
+
+    public void validateDuplicateEmail(String email) {
+        boolean exists = userRepository.existsByEmail(email);
+        if (exists) {
+            throw new CustomException(ErrorCode.EMAIL_ALREADY_EXISTS);
+        }
     }
 }
--- src/main/java/mini/delivery/global/error/ErrorCode.java
+++ src/main/java/mini/delivery/global/error/ErrorCode.java
@@ -3,12 +3,17 @@ package mini.delivery.global.error;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Getter
 public enum ErrorCode {
 
-    USER_NOT_FOUND(NOT_FOUND, "사용자를 찾을 수 없습니다.");
+    // 404 NOT_FOUND
+    USER_NOT_FOUND(NOT_FOUND, "사용자를 찾을 수 없습니다."),
+
+    // 409 CONFLICT
+    EMAIL_ALREADY_EXISTS(CONFLICT, "이미 사용 중인 이메일입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;

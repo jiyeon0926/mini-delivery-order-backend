52f17c2487e7282d5038647fcfdf40487a554691
jiyeon0926 <yeoni9094@naver.com>
2026-04-09 23:40:42 +0900

    feat: 회원탈퇴 API 구현
    - Soft Delete
    - UsernameNotFoundException 핸들러 추가

--- src/main/java/mini/delivery/domain/user/entity/User.java
+++ src/main/java/mini/delivery/domain/user/entity/User.java
@@ -70,4 +70,8 @@ public class User extends BaseEntity {
     public void updateNickname(String nickname) {
         this.nickname = nickname;
     }
+
+    public void delete() {
+        this.isDeleted = true;
+    }
 }

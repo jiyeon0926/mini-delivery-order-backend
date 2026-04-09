49cb3fc933396678caa6dcf70c69800dfc15ef03
jiyeon0926 <yeoni9094@naver.com>
2026-04-09 19:03:19 +0900

    feat: JWT 인증 관련 클래스 구현
    - UserDetailsImpl
    - UserDetailsServiceImpl
    - JwtProvider
    - JwtAuthFilter
    
    #10

---
+++ src/main/java/mini/delivery/global/auth/UserDetailsImpl.java
@@ -0,0 +1,54 @@
+package mini.delivery.global.auth;
+
+import lombok.RequiredArgsConstructor;
+import mini.delivery.domain.user.entity.User;
+import mini.delivery.global.common.enums.Role;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Role role = user.getRole();
+
+        return new ArrayList<>(role.getAuthorities());
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}

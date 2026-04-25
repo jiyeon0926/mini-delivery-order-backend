b96d50a634145ba29ea119fcb6b5f935bf94b3be
jiyeon0926 <yeoni9094@naver.com>
2026-04-25 22:33:47 +0900

    refactor: 가게 단건 조회 메뉴 응답 DTO에 menuId 필드 추가

--- src/main/java/mini/delivery/domain/store/dto/StoreMenuResponseDto.java
+++ src/main/java/mini/delivery/domain/store/dto/StoreMenuResponseDto.java
@@ -8,16 +8,18 @@ import java.util.List;
 @Getter
 public class StoreMenuResponseDto {
 
+    private final Long menuId;
     private final String menuName;
     private final int price;
 
-    private StoreMenuResponseDto(String menuName, int price) {
+    private StoreMenuResponseDto(Long menuId, String menuName, int price) {
+        this.menuId = menuId;
         this.menuName = menuName;
         this.price = price;
     }
 
     public static StoreMenuResponseDto from(Menu menu) {
-        return new StoreMenuResponseDto(menu.getName(), menu.getPrice());
+        return new StoreMenuResponseDto(menu.getId(), menu.getName(), menu.getPrice());
     }
 
     public static List<StoreMenuResponseDto> from(List<Menu> menus) {
